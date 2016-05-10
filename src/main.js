b890d4b91a0ea374c25c3efc1847aab9de22b5cb
Matthew Voss <mattv@tamu.edu>
2016-05-10 15:46:10 -0500

    updates to support build servers

--- src/main.js
+++ src/main.js
@@ -1,8 +1,76 @@
 const electron = require('electron');
 const app = electron.app;
+
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
 const BrowserWindow = electron.BrowserWindow;
 const package = require('./package.json');
-const winSquirrelStartupEventHandler = require('./js/winSquirrelStartupEventHandler');
 
 // Report crashes to our server.
 //require('crash-reporter').start();
@@ -10,44 +78,42 @@ const winSquirrelStartupEventHandler = require('./js/winSquirrelStartupEventHand
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
 let mainWindow = null;
-if (!winSquirrelStartupEventHandler.handleStartupEvent()) {
-  function createWindow () {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: package.window.width, height: package.window.height});
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: package.window.width, height: package.window.height});
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/templates/index.html');
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/templates/index.html');
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      mainWindow = null;
-    });
-  }
-
-  // Quit when all windows are closed.
-  app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin')
-      app.quit();
-    }
-  );
-
-  // This method will be called when Electron has done everything
-  // initialization and ready for creating browser windows.
-  app.on('ready', createWindow);
-
-  app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-      createWindow();
-    }
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
   });
 }
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin')
+    app.quit();
+  }
+);
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', createWindow);
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
