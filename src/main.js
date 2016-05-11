d609f7d0bd3673cf205e402ac535cebdf98b38e9
Matthew Voss <mattv@tamu.edu>
2016-05-11 10:59:33 -0500

    travis, window state updates

--- src/main.js
+++ src/main.js
@@ -1,75 +1,8 @@
+if(require('electron-squirrel-startup')) return;
 const electron = require('electron');
 const app = electron.app;
-
-// this should be placed at top of main.js to handle setup events quickly
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
-
-function handleSquirrelEvent() {
-  if (process.argv.length === 1) {
-    return false;
-  }
-
-  const ChildProcess = require('child_process');
-  const path = require('path');
-
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-  const exeName = path.basename(process.execPath);
-
-  const spawn = function(command, args) {
-    let spawnedProcess, error;
-
-    try {
-      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
-    } catch (error) {}
-
-    return spawnedProcess;
-  };
-
-  const spawnUpdate = function(args) {
-    return spawn(updateDotExe, args);
-  };
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-      // Optionally do things such as:
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-uninstall':
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-obsolete':
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-
-      app.quit();
-      return true;
-  }
-};
-
 const BrowserWindow = electron.BrowserWindow;
+const windowStateKeeper = require('electron-window-state');
 const package = require('./package.json');
 
 // Report crashes to our server.
@@ -79,9 +12,18 @@ const package = require('./package.json');
 // be closed automatically when the javascript object is GCed.
 let mainWindow = null;
 function createWindow () {
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: package.window.width,
+    defaultHeight: package.window.height
+  });
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: package.window.width, height: package.window.height});
-
+  mainWindow = new BrowserWindow({
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height
+  });
+  mainWindowState.manage(mainWindow);
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/templates/index.html');
 
